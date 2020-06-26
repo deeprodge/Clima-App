add0e2c1c9570d99c62da36be952784033f8c77b
Deep Rodge <deeprodge14@gmail.com>
2020-06-27 02:48:08 +0530

    commit 2

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,45 +4,52 @@ import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+const apiKey='549e1e365cc693bebbe0acf78c74f729';
 
 
 class LoadingScreen extends StatefulWidget {
 
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
-}
-
-
-void getLocation() async {
-    Location location= Location();
-   await location.getCurrentLocation();
-   print(location.latitude);
-   print(location.longitude);
 
 }
-void getData() async {
-  http.Response response=await http.get(
-      'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
 
-  if(response.statusCode==200){
-    String data= response.body;
-    double temp=jsonDecode(data)['main']['temp'];
-    int condition=jsonDecode(data)['weather'][0]['id'];
-    String cityName=jsonDecode(data)['name'];
-    //String weatherCondition=jsonDecode(data)['weather'][0]['description'];
-
-    print(temp);
-  }
-  else
-    print(response.statusCode);
-
-}
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
 
   @override
   void initState() {
     super.initState();
     getLocation();
+  }
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      double tempInK = jsonDecode(data)['main']['temp'];
+      double temp=((tempInK-32)*5)/9;
+      int condition = jsonDecode(data)['weather'][0]['id'];
+      String cityName = jsonDecode(data)['name'];
+      String weatherCondition = jsonDecode(data)['weather'][0]['description'];
+
+      print(temp);
+      print(condition);
+      print(cityName);
+      print(weatherCondition);
+    }
+    else
+      print(response.statusCode);
   }
   @override
   Widget build(BuildContext context) {
